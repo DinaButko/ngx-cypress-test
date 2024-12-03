9a0a73d5c3be8ed72badbaaf6af106042e1d89f0
DinaButko <butkodina@gmail.com>
2024-12-02 22:33:12 -0500

    added checkboxes

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -62,7 +62,7 @@ describe('First test suit ', () => {
             .click()
     })
 
-    it.only ('save subject of the command', () => {
+    it ('save subject of the command', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -84,6 +84,65 @@ describe('First test suit ', () => {
             cy.wrap(usingTheGridForm).find('[for=inputEmail1').should('contain', 'Email')
             cy.wrap(usingTheGridForm).find('[for=inputPassword2').should('contain', 'Password')
         })
+    })
+
+    it ('extract text value' , () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // 1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        // 2
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            const labelText = label.text()
+            expect(labelText).to.equal('Email address')
+            cy.wrap(labelText).should('contain', 'Email address')
+        })
+
+        // 3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).to.equal('Email address')
+        })
+
+        // 4 
+        cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then(classValue => {
+            expect(classValue).to.equal('label')
+        })
+
+        // 5 invoke property (e.x. input text in input field)
+        cy.get('#exampleInputEmail1').type('test@test.com')
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com').then(property => {
+            expect(property.to.equal('test@test.com'))
+        })
+
+    })
+
+    it ('radio buttons', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            // if element isn'tvisible - we can use force: true
+            cy.wrap(radioButtons).eq(0).check({force: true}).should('be.checked')
+            cy.wrap(radioButtons).eq(1).check({force: true}).should('be.checked')
+            cy.wrap(radioButtons).eq(0).should('not.be.checked')
+            cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+
+    })
+
+    it.only('checkboxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').uncheck({force: true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+        cy.get('[type="checkbox"]').eq(1).click({force:true})
     })
 
 })
